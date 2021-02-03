022734d5bddf606b5a088a293f2457e1875ee46e
David Lyon <dauglyon@users.noreply.github.com>
2021-02-02 21:24:42 -0800

    Updates

---
+++ precache-manifest.4f6bb78e3e7ec03aa05241c722470f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a364f54a90fdcebc29a0e9d588cc2a64",
+    "url": "/care-instructions/index.html"
+  },
+  {
+    "revision": "266ec3e19edaae75b135",
+    "url": "/care-instructions/static/css/main.fff6a692.chunk.css"
+  },
+  {
+    "revision": "0497d586a5e387614713",
+    "url": "/care-instructions/static/js/2.63fd3508.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/care-instructions/static/js/2.63fd3508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266ec3e19edaae75b135",
+    "url": "/care-instructions/static/js/main.dbc889a6.chunk.js"
+  },
+  {
+    "revision": "5e8e1b5f9b1a97efe9cd",
+    "url": "/care-instructions/static/js/runtime-main.2692423a.js"
+  }
+]);
